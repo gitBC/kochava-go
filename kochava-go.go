367267dbf0d46c3c928b5da069515287f85ae68e
Brian Crist <bcrist@yetanothertech.com>
2017-08-31 14:52:41 -0700

    Add godotenv to protect variables

--- kochava-go.go
+++ kochava-go.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("hello");
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	RedisServer := os.Getenv("REDIS_SERVER")
+
+	fmt.Println(RedisServer)
 }
