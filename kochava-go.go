049bd6a086f3e6962905942c1ed54f288e13be63
Brian Crist <bcrist@yetanothertech.com>
2017-08-31 16:42:03 -0700

    Print functional connection to redis
    I needed to ensure that both protected mode was off and that there was
    no bind port set on the redis server

--- kochava-go.go
+++ kochava-go.go
@@ -5,7 +5,9 @@ import (
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"github.com/go-redis/redis"
 )
+
 
 func main() {
 	err := godotenv.Load()
@@ -14,6 +16,16 @@ func main() {
 	}
 
 	RedisServer := os.Getenv("REDIS_SERVER")
+	RedisPort := os.Getenv("REDIS_PORT")
 
 	fmt.Println(RedisServer)
+	client := redis.NewClient(&redis.Options{
+		Addr:     RedisServer + ":" + RedisPort,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+
 }
