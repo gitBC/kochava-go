de4d7463d3b23574fbaa6855c91159f5338529f3
Brian Crist <bcrist@yetanothertech.com>
2017-09-01 08:31:27 -0700

    Deliver both post and get requests to location
    Will no read a random key from the queue, get the locaction and method,
    then send the request with the proper method ype to the location.
    
    It still needs to log time and delivery status. there is also ZERO in
    the way of error checking

--- kochava-go.go
+++ kochava-go.go
@@ -6,8 +6,9 @@ import (
 	"log"
 	"os"
 	"github.com/go-redis/redis"
-	"reflect"
 	"encoding/json"
+	"net/http"
+	"bytes"
 )
 
 
@@ -30,18 +31,40 @@ func main() {
 	Get a random value from a key on the stack
 	 */
 	Key := client.RandomKey().Val()
-
 	Va := client.Get(Key).Val()
 
-	fmt.Println(reflect.TypeOf(Va))
-
 	byt := []byte(Va)
-	//We need to provide a variable where the JSON package can put the decoded data. This map[string]interface{} will hold a map of strings to arbitrary data types.
+
+	/*
+	We need to provide a variable where the JSON package can put the decoded data. This map[string]interface{} will hold a map of strings to arbitrary data types.
+	 */
 	var dat map[string]interface{}
 	if err := json.Unmarshal(byt, &dat); err != nil {
 		panic(err)
 	}
-	fmt.Println(dat)
 
-	//fmt.Println(Value)
+	QueueMethod := dat["method"]
+	QueueLocation := dat["location"]
+
+
+
+	//Create http client to work on Redis Items
+	HttpClient := &http.Client{}
+
+	req, _ := http.NewRequest(QueueMethod.(string), QueueLocation.(string), nil)
+	req.Header.Add("Accept", "application/json")
+	resp, err := HttpClient.Do(req)
+
+	defer resp.Body.Close()
+
+
+	//Create a buffer to read the response body
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+
+	//Create and print string from response body
+	RequestBody := buf.String()
+	fmt.Println(RequestBody)
+
+
 }
